fe678b4b6816c3dca708e29723b43a44272eda74
Ashton Humphrey <ashtonreedhumphrey@gmail.com>
2023-04-27 23:44:51 -0400

    Initial Commit

---
+++ acres/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  acres
+//
+//  Created by Ashton Reed Humphrey on 4/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
